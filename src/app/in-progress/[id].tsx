e291354d08c56826d2268aa9cfc042c2e4849834
Enos Andrade <enosads@gmail.com>
2026-01-04 14:39:38 -0300

    Add `Transaction` component and integrate it into InProgress screen

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,10 +1,28 @@
 import { useLocalSearchParams } from 'expo-router'
 import { StatusBar, View } from 'react-native'
+import { List } from '@/components/List'
 import { PageHeader } from '@/components/PageHeader'
 import { Progress } from '@/components/Progress'
+import { Transaction, type TransactionProps } from '@/components/Transaction'
+import { TransactionTypes } from '@/utils/TransactionTypes'
 
 const details = { current: 'R$ 580,00', target: 'R$ 1.780,00', percentage: 25 }
 
+const transactions: TransactionProps[] = [
+  {
+    id: '1',
+    value: 'R$ 20,00',
+    date: '12/04/25',
+    type: TransactionTypes.Output,
+  },
+  {
+    id: '2',
+    value: 'R$ 300,00',
+    date: '12/04/25',
+    description: 'CDB de 110% no banco XPTO',
+    type: TransactionTypes.Input,
+  },
+]
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>()
   return (
@@ -18,6 +36,13 @@ export default function InProgress() {
         }}
       />
       <Progress data={details} />
+      <List
+        title="Transações"
+        data={transactions}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+      />
     </View>
   )
 }
