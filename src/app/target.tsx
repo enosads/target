fc05095ded035e7eda01b9c07fdf7b2f93fa49b2
Enos Andrade <enosads@gmail.com>
2026-01-04 13:31:56 -0300

    Add `Input` component and integrate it into Target screen

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,10 +1,19 @@
 import { View } from 'react-native'
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
 import { PageHeader } from '@/components/PageHeader'
 
 export default function Target() {
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader title="Meta" />
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input
+          label="Nova meta"
+          placeholder="Ex: Viagem para praia, Apple Watch"
+        />
+        <Button title="Salvar" />
+      </View>
     </View>
   )
 }
