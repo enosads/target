40749af1a68061383460e8783663b2c034925280
Enos Andrade <enosads@gmail.com>
2026-01-04 15:08:21 -0300

    Add `TransactionType` selector to `Transaction` screen and implement styles

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,11 +1,16 @@
 import { useLocalSearchParams } from 'expo-router'
+import { useState } from 'react'
 import { View } from 'react-native'
 import { Button } from '@/components/Button'
 import { CurrencyInput } from '@/components/CurrencyInput'
 import { Input } from '@/components/Input'
 import { PageHeader } from '@/components/PageHeader'
+import { TransactionType } from '@/components/TransactionType'
+import { TransactionTypes } from '@/utils/TransactionTypes'
 
 export default function Transaction() {
+  const [type, setType] = useState(TransactionTypes.Input)
+
   const params = useLocalSearchParams<{ id: string }>()
   return (
     <View style={{ flex: 1, padding: 24 }}>
@@ -14,6 +19,7 @@ export default function Transaction() {
         subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
       />
       <View style={{ marginTop: 32, gap: 24 }}>
+        <TransactionType selected={type} onChange={setType} />
         <CurrencyInput label="Valor (R$)" value={0} />
 
         <Input
